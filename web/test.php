beaf69bd9dd0ed054f7fe400b9a9421696028e2d
tayhaowen <tay_haowen@hotmail.com>
2022-07-08 12:31:12 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,8 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+<title>Test</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Put your description here.">
 </head>
 <body>
   Hello World!
 </body>
 </html>
+
 
