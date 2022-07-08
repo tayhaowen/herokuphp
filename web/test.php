1b4a980d069060f70374a7f24f1240be27f7a0b3
tayhaowen <tay_haowen@hotmail.com>
2022-07-08 12:12:57 +0800

    commit

--- web/test.php
+++ web/test.php
@@ -1,12 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-<title>Test</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-  <meta name="description" content="Put your description here.">
-
 </head>
 <body>
   Hello World!
 </body>
 </html>
+
