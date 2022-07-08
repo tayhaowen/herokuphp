30c0b51012b2e4ce88f6909e58bc52dfe9ebb2c3
tayhaowen <tay_haowen@hotmail.com>
2022-07-08 12:00:07 +0800

    commite

--- web/test.php
+++ web/test.php
@@ -1,5 +1,10 @@
+<!DOCTYPE html>
 <html lang="en">
 <head>
+<title>Test</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Put your description here.">
+
 </head>
 <body>
   Hello World!
